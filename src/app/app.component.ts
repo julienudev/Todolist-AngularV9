54d57a1d569a69cd5820d80d4323a65d551c0dd0
julienudev <julien.udev@gmail.com>
2020-01-27 21:01:50 +0100

    service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'Todo2020';
+  title = "Todo2020";
+  loginForm = new FormGroup({
+    login: new FormControl(""),
+    password: new FormControl("")
+  });
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.loginForm.value);
+  }
 }
